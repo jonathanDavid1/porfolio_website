528b232f09b4326afe2c9abcef3902fe9bbd478f
Jonathan David <jhernadezcorrea@gmail.com>
2023-08-03 19:56:14 -0500

    starting parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Parallax from './components/Parallax'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Parallax />
     </>
   )
 }
---
+++ src/components/Parallax.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Parallax = () => {
+  return (
+    <section className='parallax'>
+        <h2 id='text'>Parallax Website</h2>
+    </section>
+  )
+}
+
+export default Parallax
